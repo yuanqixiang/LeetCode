b97ce0b57b7e27115d1f32527f6e5d6b8e42b653
袁其祥 <yuanqixiang@meituan.com>
2019-05-14 14:43:38 +0800

    refactor ListUtils

--- src/main/java/leetcode/linkedList/utils/ListUtils.java
+++ src/main/java/leetcode/linkedList/utils/ListUtils.java
@@ -5,6 +5,7 @@ import leetcode.dataStructures.ListNode;
 public class ListUtils {
     private static final String JOINT_STRING = " --> ";
     private static final String EMPTY_STRING = "";
+
     public static String printList(ListNode head) {
         if (head == null) return EMPTY_STRING;
         ListNode curr = head;
