aa7038d4c41de6fea598b1369cd9b36820036b21
袁其祥 <yuanqixiang@meituan.com>
2019-05-14 10:50:49 +0800

    git tool

--- git.sh
+++ git.sh
@@ -1,9 +1,26 @@
 #! /bin/bash
+if [ $# != 2 ] ; then 
+    echo "USAGE: $0 file_name commit_message" 
+    echo "e.g.: $0 file.java \"some info\"" 
+    exit 1; 
+fi 
 
-git add .
+file_name=$1
+if  [ ! -n "$file_name" ] ;then
+    echo "no file is appointed"
+else
+    echo "git add $file_name"
+fi
 
-commit=$1
-echo "commit message: $commit"
-git commit -m "$commit"
+git add $file_name
+
+commit_message=$2
+if  [ ! -n "$commit_message" ] ;then
+    echo "no commit message"
+else    
+    echo "commit message: $commit_message"
+fi
+
+git commit -m "$commit_message"
 
 git push
