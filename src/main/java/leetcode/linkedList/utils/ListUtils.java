fcebce82e90370da5e592dc3f88ff76ce864d94b
袁其祥 <yuanqixiang@meituan.com>
2019-05-14 14:42:48 +0800

    refactor ListUtils

--- src/main/java/leetcode/linkedList/utils/ListUtils.java
+++ src/main/java/leetcode/linkedList/utils/ListUtils.java
@@ -3,15 +3,17 @@ package leetcode.linkedList.utils;
 import leetcode.dataStructures.ListNode;
 
 public class ListUtils {
+    private static final String JOINT_STRING = " --> ";
+    private static final String EMPTY_STRING = "";
     public static String printList(ListNode head) {
         if (head == null) return "";
         ListNode curr = head;
         StringBuilder sb = new StringBuilder();
         while (curr != null) {
-            sb.append(curr.val).append(" --> ");
+            sb.append(curr.val).append(JOINT_STRING);
             curr = curr.next;
         }
-        sb.replace(sb.length() - 5, sb.length(), "");
+        sb.replace(sb.length() - JOINT_STRING.length(), sb.length(), EMPTY_STRING);
         return sb.toString();
     }
 }
