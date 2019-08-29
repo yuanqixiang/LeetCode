be8662bdf92be9fe1ed50a1338c7b308d08be412
袁其祥 <yuanqixiang@meituan.com>
2019-08-29 16:55:11 +0800

    BinaryTreeLongestConsecutiveSequence

--- src/main/java/leetcode/tree/_0298_BinaryTreeLongestConsecutiveSequence.java
+++ src/main/java/leetcode/tree/_0298_BinaryTreeLongestConsecutiveSequence.java
@@ -3,6 +3,34 @@ package leetcode.tree;
 import leetcode.dataStructures.TreeNode;
 import org.junit.Test;
 
+/*
+ * Given a binary tree, find the length of the longest consecutive sequence path.
+ *
+ * The path refers to any sequence of nodes from some starting node to any node in
+ * the tree along the parent-child connections.
+ * The longest consecutive path need to be from parent to child (cannot be the reverse).
+ *
+ * For example,
+ *
+ *    1
+ *     \
+ *      3
+ *     / \
+ *    2   4
+ *         \
+ *          5
+ * Longest consecutive sequence path is 3-4-5, so return 3.
+ *
+ *    2
+ *     \
+ *      3
+ *     /
+ *    2
+ *   /
+ *  1
+ * Longest consecutive sequence path is 2-3, not 3-2-1, so return 2.
+ *
+ */
 public class _0298_BinaryTreeLongestConsecutiveSequence {
     private int res = 0;
     public int longestConsecutive(TreeNode root) {
